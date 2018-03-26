2cbcd9f1eff0abf4609cd994de9c87e82fd27578
Chase Bowden and Masen Matthews <chase@>
2018-03-26 14:11:34 -0700

    Build initial app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { Task } from './models/task.model'
 @Component({
-  selector: 'app-root',
+  selector: 'poop-link',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  currentFocus: string = 'Masen is Cool';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+
+  firstTask: Task = new Task("Finish Big Boy Homework over the weekend");
 }
